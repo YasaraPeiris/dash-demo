444c16a638a2cc493471ca809836426805691314
Yasara <peirisyasara@gmail.com>
2021-03-24 22:30:26 +0530

    added pytest

---
+++ server-api/app/test_main.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_read_root(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_read_item(self):
+        self.fail()
